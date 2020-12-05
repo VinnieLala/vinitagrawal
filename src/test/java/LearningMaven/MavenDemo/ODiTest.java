ae7b7d0a5db92978b40bef8a0ace93ce4ce92658
hp <agrawalvinit69@gmail.com>
2020-12-05 14:26:17 +0530

    first commit

---
+++ src/test/java/LearningMaven/MavenDemo/ODiTest.java
@@ -0,0 +1,14 @@
+package LearningMaven.MavenDemo;
+
+import org.testng.annotations.Test;
+
+public class ODiTest {
+	
+	@Test
+	
+	public void OneDayInternational() {
+		
+		System.out.println("Its a one day match");
+	}
+
+}
---
+++ src/test/java/LearningMaven/MavenDemo/TestMatchTest.java
@@ -0,0 +1,21 @@
+package LearningMaven.MavenDemo;
+
+import org.testng.annotations.Test;
+
+public class TestMatchTest {
+	
+	@Test
+	
+	public void fivedays() {
+		
+		System.out.println("Its a five day match");
+		
+		
+	}
+	@Test
+	public void NinetyOvers() {
+		
+		System.out.println("It consists of 90 overs per day");
+	}
+
+}
