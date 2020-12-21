5b96fe23bb58781bed284d9acd5393e46342c27f
Vinit Agrawal <agrawalvinit69@gmail.com>
2020-12-21 15:40:49 +0530

    New lines added

--- src/test/java/LearningMaven/MavenDemo/ODiTest.java
+++ src/test/java/LearningMaven/MavenDemo/ODiTest.java
@@ -9,6 +9,8 @@ public class ODiTest {
 	public void OneDayInternational() {
 		
 		System.out.println("Its a one day match");
+		System.out.println("Its a day night match");
+		System.out.println("it will start at 2:30 pm");
 	}
 
 }
